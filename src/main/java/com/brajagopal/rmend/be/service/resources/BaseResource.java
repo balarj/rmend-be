dc3f1757534dd6866e7b49dc42e24bdda1276351
Bala Rajagopal <bxr4261@rit.edu>
2015-07-04 22:19:19 -0700

    Refactored the BaseResource class - added the getCredentials() method for generating the GoogleCredential instance. Also refactored the getDao() method instance

--- src/main/java/com/brajagopal/rmend/be/service/resources/BaseResource.java
+++ src/main/java/com/brajagopal/rmend/be/service/resources/BaseResource.java
@@ -3,8 +3,14 @@ package com.brajagopal.rmend.be.service.resources;
 import com.brajagopal.rmend.be.recommender.ContentRecommender;
 import com.brajagopal.rmend.dao.GCloudDao;
 import com.brajagopal.rmend.dao.IRMendDao;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.services.datastore.client.DatastoreOptions;
 import org.apache.log4j.Logger;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
@@ -13,28 +19,47 @@ import java.security.GeneralSecurityException;
  */
 public class BaseResource {
 
-    private static final Logger logger = Logger.getLogger(BaseResource.class);
+    private final Logger logger = Logger.getLogger(BaseResource.class);
     private static IRMendDao dao;
     private static ContentRecommender contentRecommender;
-
-    static {
-        try {
-            dao = new GCloudDao();
-        } catch (GeneralSecurityException e) {
-            logger.warn(e);
-        } catch (IOException e) {
-            logger.warn(e);
-        }
-    }
+    private static final String SERVICE_ACCOUNT_EMAIL = "777065455744-gqlc8dar2us2amkcig46lt0fffrarlqc@developer.gserviceaccount.com";
 
     public IRMendDao getDao() {
+        if (dao == null) {
+            try {
+                logger.info("Generating a new DAO instance");
+                dao = new GCloudDao(getCredentials());
+            } catch (GeneralSecurityException e) {
+                logger.warn(e);
+            } catch (IOException e) {
+                logger.warn(e);
+                e.printStackTrace();
+            }
+        }
         return dao;
     }
 
-    public ContentRecommender getRecommender() {
+    public ContentRecommender getRecommender() throws GeneralSecurityException, IOException {
         if (contentRecommender == null) {
-            contentRecommender = new ContentRecommender(dao);
+            logger.info("Generating a new ContentRecommender instance");
+            contentRecommender = new ContentRecommender(getDao());
         }
         return contentRecommender;
+    }
+
+    public GoogleCredential getCredentials() throws GeneralSecurityException, IOException {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JacksonFactory jsonFactory = new JacksonFactory();
+        File fCredPk = new File("conf/rmend-be.p12");
+        logger.info(fCredPk.getAbsolutePath());
+
+        GoogleCredential credential = new GoogleCredential.Builder().setTransport(httpTransport)
+                .setJsonFactory(jsonFactory)
+                .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
+                .setServiceAccountScopes(DatastoreOptions.SCOPES)
+                .setServiceAccountPrivateKeyFromP12File(fCredPk)
+                .build();
+
+        return credential;
     }
 }
