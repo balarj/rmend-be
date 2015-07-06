f1da6a970a2417cc8cd67268c96b09e2d74512a1
Bala Rajagopal <bxr4261@rit.edu>
2015-07-06 06:07:41 -0700

    Added the placeholder for the CFRecommenderResource (collab-filtering)

---
+++ src/main/java/com/brajagopal/rmend/be/service/resources/CFRecommenderResource.java
@@ -0,0 +1,40 @@
+package com.brajagopal.rmend.be.service.resources;
+
+import org.apache.log4j.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author <bxr4261>
+ */
+@Path("v1/recommend/cf")
+public class CFRecommenderResource extends BaseResource {
+
+    static Logger logger = Logger.getLogger(CFRecommenderResource.class);
+
+    @Context
+    Request request;
+
+    @Produces("application/json")
+    @Path("/hello")
+    public String sayHello() {
+        return "Hello from '"+getClass().getSimpleName() + "'";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{docNumber}")
+    public Response getRecommendation(@PathParam("docNumber") long docNumber) {
+        Response.Status responseStatus = Response.Status.NOT_IMPLEMENTED;
+        String errorMsg = "NA";
+
+        return Response.status(responseStatus).header("X-Error-Msg", errorMsg).build();
+    }
+}
