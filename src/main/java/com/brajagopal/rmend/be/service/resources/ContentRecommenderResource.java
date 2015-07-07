228d23afe32b9965ccdfad46b32194e821d8e9b7
Bala Rajagopal <bxr4261@rit.edu>
2015-07-07 02:23:23 -0700

    Updated the ContentRecommenderResouce - provided a mechanism to pass the resultType as a Query param for the getRecommendation() method.

--- src/main/java/com/brajagopal/rmend/be/service/resources/ContentRecommenderResource.java
+++ src/main/java/com/brajagopal/rmend/be/service/resources/ContentRecommenderResource.java
@@ -1,15 +1,12 @@
 package com.brajagopal.rmend.be.service.resources;
 
-import com.brajagopal.rmend.be.recommender.ContentRecommender;
+import com.brajagopal.rmend.data.ResultsType;
 import com.brajagopal.rmend.data.beans.DocumentBean;
 import com.brajagopal.rmend.exception.DocumentNotFoundException;
 import com.google.api.services.datastore.client.DatastoreException;
 import org.apache.log4j.Logger;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
@@ -17,7 +14,6 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InvalidClassException;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -34,17 +30,27 @@ public class ContentRecommenderResource extends BaseResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/document/{docNumber}")
-    public Response getRecommendation(@PathParam("docNumber") long docNumber) {
+    public Response getRecommendation(@PathParam("docNumber") long docNumber, @DefaultValue("RANDOM_10") @QueryParam("resultType") String resultsTypeAsString) {
         Response.Status responseStatus = Response.Status.INTERNAL_SERVER_ERROR;
 
-        String errorMsg = "NA";
-        Collection<DocumentBean> retVal = new ArrayList<>();
+        ResultsType resultsType = ResultsType.DEFAULT_RESULT_TYPE;
         try {
-            retVal = getRecommender().getSimilarContent(docNumber, ContentRecommender.ResultsType.TOP_5);
+            resultsType = ResultsType.valueOf(resultsTypeAsString);
+        } catch (IllegalArgumentException e) {}
+
+        String errorMsg = "NA";
+        try {
+            Collection<DocumentBean> retVal = getRecommender().getSimilarContent(docNumber, resultsType);
             if (retVal.isEmpty()) {
-                return Response.status(Response.Status.NOT_FOUND).build();
+                return Response.status(
+                        Response.Status.NOT_FOUND)
+                        .header("X-Result-Type", resultsType)
+                        .build();
             }
-            return Response.ok().entity(retVal).build();
+            return Response.ok()
+                    .entity(retVal)
+                    .header("X-Result-Type", resultsType)
+                    .build();
         } catch (DatastoreException e) {
             logger.warn(e);
         } catch (InvalidClassException e) {
@@ -62,6 +68,9 @@ public class ContentRecommenderResource extends BaseResource {
             logger.warn(e);
         }
 
-        return Response.status(responseStatus).header("X-Error-Msg", errorMsg).build();
+        return Response.status(responseStatus)
+                .header("X-Error-Msg", errorMsg)
+                .header("X-Result-Type", resultsType)
+                .build();
     }
 }
