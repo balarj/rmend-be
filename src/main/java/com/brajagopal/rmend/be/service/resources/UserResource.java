0c7ccc490abd1d7a50a40871b27fa83a7769f1ae
Bala Rajagopal <bxr4261@rit.edu>
2015-07-11 21:59:53 -0700

    Added the getUserByUUID() and putUserView() endpoints to the User resource.

--- src/main/java/com/brajagopal/rmend/be/service/resources/UserResource.java
+++ src/main/java/com/brajagopal/rmend/be/service/resources/UserResource.java
@@ -1,18 +1,20 @@
 package com.brajagopal.rmend.be.service.resources;
 
 import com.brajagopal.rmend.app.beans.UserBean;
+import com.brajagopal.rmend.app.beans.UserViewBean;
 import com.brajagopal.rmend.be.entities.UserEntity;
+import com.brajagopal.rmend.be.entities.ViewEntity;
 import com.brajagopal.rmend.exception.DuplicateEntryException;
 import org.apache.log4j.Logger;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * @author <bxr4261>
@@ -27,14 +29,13 @@ public class UserResource extends BaseResource {
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{userName}")
+    @Path("/uname/{userName}")
     public Response createUser(@PathParam("userName") String userName) {
 
         Response.ResponseBuilder response = Response.serverError();
 
         UserBean userBean = UserBean.create(userName);
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("users-entity");
-        EntityManager manager = factory.createEntityManager();
+        EntityManager manager = getEntityManager("users-entity");
         UserEntity user;
         try {
             manager.getTransaction().begin();
@@ -48,15 +49,16 @@ public class UserResource extends BaseResource {
         }
         catch (DuplicateEntryException e) {
             logger.error(e);
-            manager.getTransaction().rollback();
             return response.header("X-Error-Msg", e.getMessage()).status(Response.Status.BAD_REQUEST).build();
         }
         catch (Exception e) {
             logger.error(e);
-            manager.getTransaction().rollback();
             return response.header("X-Error-Msg", e.getMessage()).status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
         finally {
+            if (manager.getTransaction().isActive()) {
+                manager.getTransaction().rollback();
+            }
             manager.close();
         }
 
@@ -66,12 +68,11 @@ public class UserResource extends BaseResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{userName}")
+    @Path("/uname/{userName}")
     public Response getUser(@PathParam("userName") String userName) {
         Response.ResponseBuilder response = Response.serverError();
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("users-entity");
-        EntityManager manager = factory.createEntityManager();
+        EntityManager manager = getEntityManager("users-entity");
         UserBean retVal;
         try {
             UserEntity userEntity = manager.find(UserEntity.class, userName);
@@ -82,7 +83,6 @@ public class UserResource extends BaseResource {
         }
         catch (Exception e) {
             logger.error(e);
-            manager.getTransaction().rollback();
             return response.header("X-Error-Msg", e.getMessage()).status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
         finally {
@@ -90,5 +90,74 @@ public class UserResource extends BaseResource {
         }
 
         return response.status(Response.Status.OK).entity(retVal).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/uuid/{uuid}")
+    public Response getUserByUUID(@PathParam("uuid") String uuId) {
+        Response.ResponseBuilder response = Response.serverError();
+
+        //TODO: Validate UUID
+        EntityManager manager = getEntityManager("users-entity");
+        UserBean retVal;
+        try {
+            Query query = manager.createQuery("SELECT u FROM "
+                    + UserEntity.class.getName()
+                    + " u WHERE u.uuid = '" + uuId + "'");
+            List<UserEntity> results = query.getResultList();
+            if (results.isEmpty()) {
+                return response.status(Response.Status.NOT_FOUND).header("X-UUID", uuId).build();
+            }
+            else if (results.size() > 1) {
+                logger.error("Too many ("+results.size()+") entities with the same UUID ("+uuId+")");
+                return response.status(Response.Status.INTERNAL_SERVER_ERROR).header("X-UUID", uuId).build();
+            }
+            retVal = results.get(0).getUserBean();
+        }
+        catch (Exception e) {
+            logger.error(e);
+            return response.header("X-Error-Msg", e.getMessage()).status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+        finally {
+            manager.close();
+        }
+
+        return response.status(Response.Status.OK).entity(retVal).build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/view")
+    public Response putUserView(UserViewBean userViewBean) {
+        Response.ResponseBuilder response = Response.serverError();
+
+        //TODO: Validate if the uid and docNum are valid
+        EntityManager manager = getEntityManager("users-entity");
+
+        try {
+            manager.getTransaction().begin();
+            ViewEntity entity = manager.find(ViewEntity.class, userViewBean.getCompositeKey());
+            if (entity != null) {
+                throw new DuplicateEntryException(userViewBean.getCompositeKey());
+            }
+            ViewEntity viewEntity = ViewEntity.createInstance(userViewBean);
+            manager.persist(viewEntity);
+            manager.getTransaction().commit();
+        }
+        catch (DuplicateEntryException e) {
+            logger.error(e);
+            return response.header("X-Error-Msg", e.getMessage()).status(Response.Status.BAD_REQUEST).build();
+        }
+        catch (Exception e) {
+            logger.error(e);
+            return response.header("X-Error-Msg", e.getMessage()).status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } finally {
+            if (manager.getTransaction().isActive()) {
+                manager.getTransaction().rollback();
+            }
+            manager.close();
+        }
+        return response.status(Response.Status.NO_CONTENT).build();
     }
 }
