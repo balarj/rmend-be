a839093f60624a8d7f88dac92514ae80d9e1bad3
Bala Rajagopal <bxr4261@rit.edu>
2015-07-15 05:49:51 -0700

    Hooked up the Recommender (BE) call for the CFRecommenderResource.

--- src/main/java/com/brajagopal/rmend/be/service/resources/CFRecommenderResource.java
+++ src/main/java/com/brajagopal/rmend/be/service/resources/CFRecommenderResource.java
@@ -1,6 +1,10 @@
 package com.brajagopal.rmend.be.service.resources;
 
+import com.brajagopal.rmend.data.ResultsType;
+import com.brajagopal.rmend.exception.DocumentNotFoundException;
+import com.google.api.services.datastore.client.DatastoreException;
 import org.apache.log4j.Logger;
+import org.apache.mahout.cf.taste.common.TasteException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,6 +14,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 /**
  * @author <bxr4261>
@@ -28,6 +34,24 @@ public class CFRecommenderResource extends BaseResource {
     public Response getRecommendation(@PathParam("docNumber") long docNumber) {
         Response.Status responseStatus = Response.Status.NOT_IMPLEMENTED;
         String errorMsg = "NA";
+
+        try {
+            getRecommender(RecommenderTypeEnum.COLLABORATIVE_FILTERING)
+                    .getRecommendation(docNumber, ResultsType.RANDOM_10);
+        } catch (DatastoreException e) {
+            logger.warn(e);
+        } catch (DocumentNotFoundException e) {
+            logger.warn(e);
+        } catch (GeneralSecurityException e) {
+            errorMsg = e.getMessage();
+            logger.warn(e);
+        } catch (IOException e) {
+            errorMsg = e.getMessage();
+            logger.warn(e);
+        } catch (TasteException e) {
+            e.printStackTrace();
+        }
+
 
         return Response.status(responseStatus).header("X-Error-Msg", errorMsg).build();
     }
