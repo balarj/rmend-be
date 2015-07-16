b5eb817161f91c1eff3587c5190e29e3abc93991
Bala Rajagopal <bxr4261@rit.edu>
2015-07-16 08:57:19 -0700

    Refactored the CFRecommenderResource to return the results for CF recommendations.

--- src/main/java/com/brajagopal/rmend/be/service/resources/CFRecommenderResource.java
+++ src/main/java/com/brajagopal/rmend/be/service/resources/CFRecommenderResource.java
@@ -1,21 +1,20 @@
 package com.brajagopal.rmend.be.service.resources;
 
 import com.brajagopal.rmend.data.ResultsType;
+import com.brajagopal.rmend.data.beans.DocumentBean;
 import com.brajagopal.rmend.exception.DocumentNotFoundException;
 import com.google.api.services.datastore.client.DatastoreException;
 import org.apache.log4j.Logger;
 import org.apache.mahout.cf.taste.common.TasteException;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Collection;
 
 /**
  * @author <bxr4261>
@@ -31,13 +30,33 @@ public class CFRecommenderResource extends BaseResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/document/{docNumber}")
-    public Response getRecommendation(@PathParam("docNumber") long docNumber) {
-        Response.Status responseStatus = Response.Status.NOT_IMPLEMENTED;
+    public Response getRecommendation(
+            @PathParam("docNumber") long docNumber,
+            @DefaultValue("RANDOM_10") @QueryParam("resultType") String resultsTypeAsString) {
+
+        Response.Status responseStatus = Response.Status.INTERNAL_SERVER_ERROR;
+        ResultsType resultsType = ResultsType.DEFAULT_RESULT_TYPE;
+        try {
+            resultsType = ResultsType.valueOf(resultsTypeAsString);
+        } catch (IllegalArgumentException e) {}
+
         String errorMsg = "NA";
 
         try {
-            getRecommender(RecommenderTypeEnum.COLLABORATIVE_FILTERING)
-                    .getRecommendation(docNumber, ResultsType.RANDOM_10);
+            Collection<DocumentBean> retVal =
+                    getRecommender(RecommenderTypeEnum.COLLABORATIVE_FILTERING)
+                            .getRecommendation(docNumber, ResultsType.RANDOM_10);
+
+            if (retVal.isEmpty()) {
+                return Response.status(
+                        Response.Status.NOT_FOUND)
+                        .header("X-Result-Type", resultsType)
+                        .build();
+            }
+            return Response.ok()
+                    .entity(retVal)
+                    .header("X-Result-Type", resultsType)
+                    .build();
         } catch (DatastoreException e) {
             logger.warn(e);
         } catch (DocumentNotFoundException e) {
@@ -53,6 +72,9 @@ public class CFRecommenderResource extends BaseResource {
         }
 
 
-        return Response.status(responseStatus).header("X-Error-Msg", errorMsg).build();
+        return Response.status(responseStatus)
+                .header("X-Error-Msg", errorMsg)
+                .header("X-Result-Type", resultsType)
+                .build();
     }
 }
