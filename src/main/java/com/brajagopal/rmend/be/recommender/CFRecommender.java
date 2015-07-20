61aefe7910c36fd25e066ebb35256a64462e8f9a
Bala Rajagopal <bxr4261@rit.edu>
2015-07-19 18:50:05 -0700

    Refactored the CFRecommender class to implement the getItemSimilarity() method. (Item similarity works great - yay)

--- src/main/java/com/brajagopal/rmend/be/recommender/CFRecommender.java
+++ src/main/java/com/brajagopal/rmend/be/recommender/CFRecommender.java
@@ -12,12 +12,18 @@ import com.google.api.services.datastore.client.DatastoreException;
 import org.apache.log4j.Logger;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.ItemBasedRecommender;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -40,15 +46,44 @@ public class CFRecommender implements IRecommender {
     }
 
     @Override
-    public RecResponseBean getRecommendation(long _documentNumber, ResultsType _resultsType)
+    public RecResponseBean getRecommendation(long _userId, ResultsType _resultsType)
             throws DatastoreException, DocumentNotFoundException, TasteException {
 
         //RecommenderIRStatsEvaluator evaluator = new GenericRecommenderIRStatsEvaluator();
-        RecommenderBuilderWrapper builder =
-                new RecommenderBuilderWrapper(new TanimotoCoefficientSimilarity(dataModel));
+        RecommendationBuilderWrapper builder =
+                new RecommendationBuilderWrapper(
+                        new PearsonCorrelationSimilarity(dataModel));
 
         List<RecommendedItem> recommendations =
                 builder.buildRecommender(dataModel).recommend(
+                        _userId, _resultsType.getDaoResultLimit());
+
+        Collection<DocumentBean> results = new ArrayList<>(recommendations.size());
+        for (RecommendedItem recommendedItem : recommendations) {
+            try {
+                results.add(dao.getDocument(recommendedItem.getItemID()));
+            }
+            catch (DocumentNotFoundException e) {
+                logger.warn(e);
+            }
+        }
+
+        RecResponseBean response = new RecResponseBean(
+                results, builder.getSimilarityClass().getSimpleName());
+
+        return response;
+    }
+
+    @Override
+    public RecResponseBean getItemSimilarity(long _documentNumber, ResultsType _resultsType)
+            throws DatastoreException, DocumentNotFoundException, TasteException {
+
+        SimilarityBuilderWrapper builder =
+                new SimilarityBuilderWrapper(
+                        new TanimotoCoefficientSimilarity(dataModel));
+
+        List<RecommendedItem> recommendations =
+                builder.buildRecommender(dataModel).mostSimilarItems(
                         _documentNumber, _resultsType.getDaoResultLimit());
 
         Collection<DocumentBean> results = new ArrayList<>(recommendations.size());
@@ -69,24 +104,50 @@ public class CFRecommender implements IRecommender {
 
     @Override
     public Collection<DocumentBean> getContentByTopic(TopicBean _topicBean, ResultsType _resultsType) throws DatastoreException, DocumentNotFoundException {
-        return null;
+        throw new UnsupportedOperationException(
+                "getContentByTopic() not supported in CFRecommender");
     }
 
-    static final class RecommenderBuilderWrapper implements RecommenderBuilder {
+    static final class SimilarityBuilderWrapper implements RecommenderBuilder {
 
         private ItemSimilarity similarity;
 
-        public RecommenderBuilderWrapper (ItemSimilarity instance) {
+        public SimilarityBuilderWrapper(ItemSimilarity instance) {
             this.similarity = instance;
         }
 
         @Override
-        public Recommender buildRecommender(DataModel dataModel) throws TasteException {
-            //similarity = new LogLikelihoodSimilarity(dataModel);
+        public ItemBasedRecommender buildRecommender(DataModel dataModel) throws TasteException {
             return new GenericBooleanPrefItemBasedRecommender(dataModel, similarity);
         }
 
         public Class<? extends ItemSimilarity> getSimilarityClass() {
+            return similarity.getClass();
+        }
+    }
+
+    static final class RecommendationBuilderWrapper implements RecommenderBuilder {
+
+        private UserSimilarity similarity;
+
+        public RecommendationBuilderWrapper(UserSimilarity instance) {
+            this.similarity = instance;
+        }
+
+        @Override
+        public UserBasedRecommender buildRecommender(DataModel dataModel) throws TasteException {
+
+            UserNeighborhood neighborhood =
+                    new ThresholdUserNeighborhood(
+                            0.1, new PearsonCorrelationSimilarity(dataModel), dataModel);
+
+            return new GenericBooleanPrefUserBasedRecommender(
+                    dataModel,
+                    neighborhood,
+                    similarity);
+        }
+
+        public Class<? extends UserSimilarity> getSimilarityClass() {
             return similarity.getClass();
         }
     }
