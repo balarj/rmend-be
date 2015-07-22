f8b349d841d34c5e2ee1320c8d2269268aab4ae6
Bala Rajagopal <bxr4261@rit.edu>
2015-07-22 04:46:01 -0700

    Added the AutomationBotTraceBean (to track Bot requests) to the mix.

--- src/main/java/com/brajagopal/rmend/be/beans/AutomationBotTraceBean.java
+++ src/main/java/com/brajagopal/rmend/be/beans/AutomationBotTraceBean.java
@@ -3,6 +3,47 @@ package com.brajagopal.rmend.be.beans;
 /**
  * @author <bxr4261>
  */
+@SuppressWarnings("unused")
 public class AutomationBotTraceBean {
 
+    private Long uid;
+    private String topic;
+    private Long docNumber;
+    private Long timestamp;
+    private Integer statusCode;
+
+    public AutomationBotTraceBean(Long _uid, Long _docNumber, String _topic, Integer _statusCodde) {
+        this.uid = _uid;
+        this.docNumber = _docNumber;
+        this.topic = _topic;
+        this.timestamp = System.currentTimeMillis();
+        this.statusCode = _statusCodde;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public Long getDocNumber() {
+        return docNumber;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    @Override
+    public String toString() {
+        return "AutomationBotTraceBean{" +
+                "uid=" + uid +
+                ", topic='" + topic + '\'' +
+                ", docNumber=" + docNumber +
+                ", timestamp=" + timestamp +
+                ", statusCode=" + statusCode +
+                '}';
+    }
 }
